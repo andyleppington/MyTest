f238eb1c34ebf82bd5a6a76a1568c89c1d195f86
aleppington <andy.leppington@quantiv.com>
2013-09-27 17:00:11 +0100

    Add box type

---
+++ BoxType.sql
@@ -0,0 +1,56 @@
+	INSERT INTO [dbo].[AC01_BoxType]
+			   ([BoxTypeId],[BoxTypeName],[BoxTypeIsDeleted]
+			   ,[BoxTypeVersion],[BoxTypeModifiedById],[BoxTypeModifiedDateTime]
+			   ,[BoxTypeCreatedById],[BoxTypeCreatedDateTime],[BoxHeight]
+			   ,[BoxDepth],[BoxWidth])
+	 VALUES
+		   (1,'Small',0
+		   ,1,2,GETDATE()
+		   ,2,GETDATE(),26.5
+		   ,32.2,11)
+
+	INSERT INTO [dbo].[AC01_BoxType]
+			   ([BoxTypeId],[BoxTypeName],[BoxTypeIsDeleted]
+			   ,[BoxTypeVersion],[BoxTypeModifiedById],[BoxTypeModifiedDateTime]
+			   ,[BoxTypeCreatedById],[BoxTypeCreatedDateTime],[BoxHeight]
+			   ,[BoxDepth],[BoxWidth])
+	 VALUES
+		   (2,'Medium',0
+		   ,1,2,GETDATE()
+		   ,2,GETDATE(),30.5
+		   ,37,13.3)
+
+	INSERT INTO [dbo].[AC01_BoxType]
+			   ([BoxTypeId],[BoxTypeName],[BoxTypeIsDeleted]
+			   ,[BoxTypeVersion],[BoxTypeModifiedById],[BoxTypeModifiedDateTime]
+			   ,[BoxTypeCreatedById],[BoxTypeCreatedDateTime],[BoxHeight]
+			   ,[BoxDepth],[BoxWidth])
+	 VALUES
+		   (3,'Large',0
+		   ,1,2,GETDATE()
+		   ,2,GETDATE(),32
+		   ,55.2,15)
+
+	INSERT INTO [dbo].[AC01_BoxType]
+			   ([BoxTypeId],[BoxTypeName],[BoxTypeIsDeleted]
+			   ,[BoxTypeVersion],[BoxTypeModifiedById],[BoxTypeModifiedDateTime]
+			   ,[BoxTypeCreatedById],[BoxTypeCreatedDateTime],[BoxHeight]
+			   ,[BoxDepth],[BoxWidth])
+	 VALUES
+		   (4,'Extra Large',0
+		   ,1,2,GETDATE()
+		   ,2,GETDATE(),73
+		   ,46.5,16)
+
+	INSERT INTO [dbo].[AC01_BoxType]
+			   ([BoxTypeId],[BoxTypeName],[BoxTypeIsDeleted]
+			   ,[BoxTypeVersion],[BoxTypeModifiedById],[BoxTypeModifiedDateTime]
+			   ,[BoxTypeCreatedById],[BoxTypeCreatedDateTime],[BoxHeight]
+			   ,[BoxDepth],[BoxWidth])
+	 VALUES
+		   (5,'Gift Envelope',0
+		   ,1,2,GETDATE()
+		   ,2,GETDATE(),31.3
+		   ,18.4,6)
+
+
